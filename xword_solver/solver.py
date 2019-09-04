8131084759631433f138d66f78952424ceb7acaf
Andrew Chau <andrewchau333@gmail.com>
2019-09-03 17:26:24 -0700

    Implemented v. poor solving algorithm for testing

--- xword_solver/solver.py
+++ xword_solver/solver.py
@@ -2,9 +2,11 @@ import json
 from neo4j import GraphDatabase
 import re
 import queue
+import html
 
 from board import Board
 from clue import Clue
+
 
 class XwordSolver():
     def __init__(self, boardfile):
@@ -17,18 +19,15 @@ class XwordSolver():
         for clue in data['clues']['across']:
             clue = clue_re.search(clue) # 1) num, 2) clue 
             self.clues[(clue.group(1), 'ACROSS')] = \
-                (Clue(clue.group(1), 'ACROSS', hint=clue.group(2), board=self.board))
+                (Clue(clue.group(1), 'ACROSS', 
+                hint=html.unescape(clue.group(2)),
+                board=self.board))
         for clue in data['clues']['down']:
             clue = clue_re.search(clue) # 1) num, 2) clue 
             self.clues[(clue.group(1), 'DOWN')] = \
-                (Clue(clue.group(1), 'DOWN', hint=clue.group(2), board=self.board))
-        # for clue in data['clues']['down']:
-        #     clue = clue_re.search(clue) # 1) num, 2) clue 
-        #     self.clues.add(Clue(clue.group(1), 'DOWN', hint=clue.group(2), board=self.board))
-        self.tosolve = queue.Queue()
-        # map(self.tosolve.put, self.clues)
-        for clue in self.clues:
-            self.tosolve.put(clue)
+                (Clue(clue.group(1), 'DOWN', 
+                hint=html.unescape(clue.group(2)),
+                board=self.board))
         uri = "bolt://localhost:7687"
         self.driver = GraphDatabase.driver(uri, auth=("xword", "xword"))
     
@@ -48,19 +47,41 @@ class XwordSolver():
 
     # first attempt at solving a board (v. basic)
     def solve(self):
-        while not self.tosolve.empty():
-            curr = self.tosolve.get(False)
+        firstpass = queue.Queue()
+        secondpass = queue.Queue()
+        count = 0
+        [firstpass.put(clue) for clue in self.clues.keys()]
+        while not firstpass.empty():
+            curr = firstpass.get(False)
             num = curr[0]
             direction = curr[1]
             matches = self.query_clue(num, direction)
-            if len(matches) == 1:
+            if not matches:
+                secondpass.put(curr)
+                count += 1
+            elif len(matches) == 1:
+                print(curr, matches[0])
                 self.board.set_word(num, direction, matches[0])
-            # else:
-                # matches = self.query_word(curr[0], curr[1])
-                # if len(matches) == 1:
-                #     print(f"{self.get_clue(curr[0], curr[1]).hint}: {matches[0]}")
-                # else:
-                    # self.tosolve.put(curr)
+            else:
+                firstpass.put(curr)
+
+        # TODO: Implement a real algorithm 
+        for _ in range(count << 1):
+            if secondpass.empty():
+                break
+            curr = secondpass.get(False)
+            num = curr[0]   
+            direction = curr[1]
+            matches = self.query_word(num, direction)
+            if not matches:
+                # probably recursive step base case
+                pass
+            elif len(matches) == 1:
+                print(curr, matches[0])
+                self.board.set_word(num, direction, matches[0])
+            else:
+                secondpass.put(curr)
+
         self.board.print_board()
         self.solution.print_board()
 
